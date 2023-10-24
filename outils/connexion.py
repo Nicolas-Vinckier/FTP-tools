dbb17fe447b257fa657d8919d413b67771c339ee
Nicolas-Vinckier <nicolas.vinckier05@gmail.com>
2023-10-24 13:57:28 +0200

    Fonction speedtest

--- outils/connexion.py
+++ outils/connexion.py
@@ -1,6 +1,7 @@
 import os
 import json
 import ftplib
+import time
 
 
 def connexion_ftp(fichier_config):
@@ -36,11 +37,26 @@ def select_server():
     return os.path.join("serveursFTP", fichiers[choix - 1])
 
 
+def speedtest(ftp):
+    # Création d'un fichier de 1 Mo
+    with open("10Mo.txt", "wb") as f:
+        f.write(b"0" * 10000000)
+
+    # Envoi du fichier sur le serveur FTP
+    ftp.storbinary("STOR 10Mo.txt", open("10Mo.txt", "rb"))
+
+    # Suppression du fichier sur le serveur FTP
+    ftp.delete("10Mo.txt")
+
+    # Suppression du fichier en local
+    os.remove("10Mo.txt")
+
+
 def menu_connexion(ftp):
     while True:
         print("Menu de Connexion:")
         print("0. Revenir au menu principal")
-        print("1. Test de ping")
+        print("1. SpeedTest")
 
         choix = input("Votre choix : ")
         contenu = []
@@ -48,7 +64,19 @@ def menu_connexion(ftp):
         if choix == "0":
             break
         elif choix == "1":
-            pass
+            start = time.time()
+            speedtest(ftp)
+            end = time.time()
+
+            # Affichage du temps d'exécution
+            print("SpeedTest terminé en", end - start, "secondes.")
+
+            # Calcul du débit
+            debit = 10 / (end - start)
+
+            # Affichage du débit
+            print("Débit : {:.2f} Mo/s".format(debit))
+
         else:
             print("Choix invalide, veuillez réessayer.")
 
