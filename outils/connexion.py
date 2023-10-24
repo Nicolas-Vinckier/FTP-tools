2411b99d26ac85c724a9903713fcd58279418593
EtapesDev <137047380+Dev-EtapesPrint@users.noreply.github.com>
2023-10-24 11:06:10 +0200

    :construction:

--- outils/connexion.py
+++ outils/connexion.py
@@ -36,35 +36,40 @@ def select_server():
     return os.path.join("serveursFTP", fichiers[choix - 1])
 
 
-def recherche_recursive(ftp, path):
-    def lister_contenu(path):
-        contenu = ftp.nlst(path)
-        list_dir = []
-        list_file = []
-        for element in contenu:
-            if ftp.nlst(element) == []:
-                list_file.append(element)
-            else:
-                list_dir.append(element)
-        return list_dir, list_file
+def lister_fichiers_dossiers(ftp, chemin="."):
+    contenu = []
 
-    def parcourir_dossiers(path):
-        list_dir, list_file = lister_contenu(path)
-        for dossier in list_dir:
-            parcourir_dossiers(dossier)
+    try:
+        # Tente de se déplacer vers le répertoire spécifié
+        ftp.cwd(chemin)
+    except ftplib.error_perm as e:  # Si une erreur de permission se produit
+        print(f"Permission refusée pour {chemin}: {str(e)}")
+        return (
+            contenu  # retourne la liste vide, car vous ne pouvez pas explorer ce chemin
+        )
 
-    list_dir, list_file = lister_contenu(path)
+    # Récupérer la liste des fichiers et dossiers dans le répertoire courant du serveur FTP
+    elements = ftp.nlst()
 
-    total_dossiers = len(list_dir)
-    total_fichiers = len(list_file)
+    for element in elements:
+        # Si c'est un dossier, ajouter le dossier à la liste et parcourir récursivement son contenu
+        if "." not in element:
+            contenu.append(element)
+            contenu.extend(lister_fichiers_dossiers(ftp, element))
+        else:
+            # Si c'est un fichier, ajouter le fichier à la liste
+            contenu.append(element)
 
-    return total_dossiers, total_fichiers
+    # Retourner au répertoire précédent pour d'autres appels récursifs
+    ftp.cwd("..")
+
+    return contenu
 
 
 def menu_connexion(ftp):
     while True:
         print("Menu de Connexion:")
-        print("1. Nombre de dossier/fichier")
+        print("1. Liste des fichiers et dossiers")
         print("2. Revenir au menu principal")
 
         choix = input("Votre choix : ")
@@ -72,11 +77,16 @@ def menu_connexion(ftp):
         if choix == "1":
             try:
                 print("Recherche en cours...")
-                total_dossiers, total_fichiers = recherche_recursive(ftp, ".")
-                print(f"Nombre de dossiers : {total_dossiers}")
-                print(f"Nombre de fichiers : {total_fichiers}")
-            except:
-                print("Erreur lors de la récupération du nombre de fichiers.")
+                contenu = lister_fichiers_dossiers(ftp)
+                print("Liste des fichiers et dossiers :")
+                for element in contenu:
+                    print(element)
+            except Exception as e:
+                print(
+                    "Erreur lors de la récupération de la liste des fichiers et dossiers :",
+                    str(e),
+                )
+            print(contenu)
         elif choix == "2":
             break
         else:
@@ -91,4 +101,4 @@ def main():
     ftp = connexion_ftp(fichier_config)
 
     # Affichage du menu de connexion
-    menu_connexion
+    menu_connexion(ftp)
